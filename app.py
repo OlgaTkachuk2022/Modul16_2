cc0da7539e05c8df757651e80db569fcf383d2c3
Olha Tkachuk <2006olgaolga2006@ukr.net>
2023-03-22 12:27:45 +0100

    dev

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,41 +9,54 @@ db = SQLAlchemy(app)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    content = db.Column(db.Text)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    published = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
-        return f'<Post {self.id}>'
+        return f'<Post {self.title}>'
 
 
 @app.route('/')
 def index():
-    posts = Post.query.all()
+    posts = Post.query.filter_by(published=True).all()
     return render_template('index.html', posts=posts)
 
 
-@app.route('/create', methods=['GET', 'POST'])
-def create():
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        post = Post(title=title, content=content)
+        published = 'published' in request.form
+        post = Post(title=title, content=content, published=published)
         db.session.add(post)
         db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('create.html')
+        return redirect('/')
+    return render_template('new.html')
 
 
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    post = Post.query.get_or_404(id)
+@app.route('/edit/<int:post_id>', methods=['GET', 'POST'])
+def edit(post_id):
+    post = Post.query.get_or_404(post_id)
     if request.method == 'POST':
         post.title = request.form['title']
         post.content = request.form['content']
+        post.published = 'published' in request.form
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect('/')
     return render_template('edit.html', post=post)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/delete/<int:post_id>', methods=['POST'])
+def delete(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
